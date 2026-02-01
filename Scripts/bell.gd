3a3f348f4f5ae41993edf053c83678654d17dbb6
Connor Klein <kleinc3@mail.uc.edu>
2026-02-01 14:47:53 -0500

    fixed bell

--- Scripts/bell.gd
+++ Scripts/bell.gd
@@ -3,9 +3,15 @@ extends TextureButton
 
 func _on_pressed() -> void:
 	var cauldron = self.get_parent().get_node("Cauldron")
-	var ingredients = cauldron.get_node("Ingredients").get_children()
-	var soup = FlavorData.new()
-	var poison = get_parent().poison
+	var ingredientsNodes : Array[Node] = cauldron.get_node("Ingredients").get_children()
+	var ingredients : Array[Ingredient]
+	for node in ingredientsNodes:
+		if typeof(node) == typeof(Ingredient):
+			var i : Ingredient = node
+			ingredients.append(i)
+			
+	var soup : FlavorData = FlavorData.new()
+	var poison : FlavorData = get_parent().poison
 	
 	# The ingredients gets captured. Call this on the newly instatiated scene
 	var setup_fn = func (x : FlavorReport): x.setup_scene(ingredients, soup, poison)

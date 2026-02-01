260ea56b33ec9786f39d99e0edcedbf09071ff3b
gyurgyma <gyurgyma@mail.uc.edu>
2026-02-01 16:56:50 -0500

    Update bell.gd
    fixed merge problem

--- Scripts/bell.gd
+++ Scripts/bell.gd
@@ -19,6 +19,9 @@ func _on_pressed() -> void:
 	var poison : FlavorData = get_parent().poison
 	
 	if get_parent().get_node("LiveTasters").tasters.size() > 0:
+		# Create report to click
+		create_flavor_report(soup, poison, ingredients)
+
 		# The ingredients gets captured. Call this on the newly instatiated scene
 		var setup_fn = func (x : FlavorReport): x.setup_scene(ingredients, soup, poison, false)
 		Signals.emit_signal("load_new_scene", "res://Scenes/flavor_report.tscn", false, setup_fn)
@@ -52,3 +55,12 @@ func _combine_flavor_data(final_soup_flavor_report: FlavorReport) -> FlavorData:
 	flavor_data.richness = final_soup_flavor_report._ingredients_flavor_data.richness + final_soup_flavor_report._base_soup_flavor_data.richness + final_soup_flavor_report._poison_flavor_data.richness
 	flavor_data.acidity = final_soup_flavor_report._ingredients_flavor_data.acidity + final_soup_flavor_report._base_soup_flavor_data.acidity + final_soup_flavor_report._poison_flavor_data.acidity
 	return flavor_data
+
+
+func create_flavor_report(soup : FlavorData, poison : FlavorData, ingredients : Array[Ingredient]):
+	var flavor_reports = get_parent().get_node("FlavorReports")
+	var flavorReportButton : ViewFlavorReport = load("res://Scenes/view_flavor_report.tscn").instantiate()
+	flavorReportButton.setup(soup, poison, ingredients)
+	flavorReportButton.custom_minimum_size.x = flavor_reports.custom_minimum_size.x
+	flavorReportButton.custom_minimum_size.y = flavor_reports.custom_minimum_size.y
+	get_parent().get_node("FlavorReports").add_child(flavorReportButton)

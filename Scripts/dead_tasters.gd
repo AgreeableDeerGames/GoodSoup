3851a6ec05f5e30eae2230f21dba6dd125feb2de
R Lavin <lavinrp@gmail.com>
2026-02-01 15:27:39 -0500

    testers can die

--- Scripts/dead_tasters.gd
+++ Scripts/dead_tasters.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
-var tasters: Array[Sprite2D]
+var hidden_testers: Array[Sprite2D]
 
 func kill_taster() -> void:
-	if tasters.size() > 0:
-		remove_child(tasters.back())
-		tasters.pop_back()
+	if hidden_testers.size() > 0:
+		hidden_testers.back().visible = true
+		hidden_testers.pop_back()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tasters = [$Taster1, $Taster2, $Taster3, $Taster4, $Taster5]
+	hidden_testers = [$Taster5, $Taster4, $Taster3, $Taster2, $Taster1 ]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:

8a44c956393a6994efd991e52da10b01022d6896
Connor Klein <kleinc3@mail.uc.edu>
2026-02-01 15:48:41 -0500

    add reports

--- Scripts/bell.gd
+++ Scripts/bell.gd
@@ -13,7 +13,20 @@ func _on_pressed() -> void:
 	var soup : FlavorData = FlavorData.new()
 	var poison : FlavorData = get_parent().poison
 	
+	# Create report to click
+	_create_flavor_report(soup, poison, ingredients)
+	
 	# The ingredients gets captured. Call this on the newly instatiated scene
 	var setup_fn = func (x : FlavorReport): x.setup_scene(ingredients, soup, poison)
 	
 	Signals.emit_signal("load_new_scene", "res://Scenes/flavor_report.tscn", false, setup_fn)
+
+func _create_flavor_report(soup : FlavorData, poison : FlavorData, ingredients : Array[Ingredient]):
+	var flavor_reports = get_parent().get_node("FlavorReports")
+	var flavorReportButton : ViewFlavorReport = load("res://Scenes/view_flavor_report.tscn").instantiate()
+	flavorReportButton.setup(soup, poison, ingredients)
+	flavorReportButton.custom_minimum_size.x = flavor_reports.custom_minimum_size.x
+	flavorReportButton.custom_minimum_size.y = flavor_reports.custom_minimum_size.y
+	get_parent().get_node("FlavorReports").add_child(flavorReportButton)
+	
+	

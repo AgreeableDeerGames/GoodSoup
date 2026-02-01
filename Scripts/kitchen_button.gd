ea9ff81da2ace467ad0abc51774798234b93782c
R Lavin <lavinrp@gmail.com>
2026-02-01 16:49:47 -0500

    fix stuff

--- Scripts/kitchen_button.gd
+++ Scripts/kitchen_button.gd
@@ -4,15 +4,15 @@ func _on_pressed() -> void:
 	# Pass the report stuff into this
 	var soup : FlavorData = FlavorData.new();
 	var parent = get_parent().get_parent().get_parent()
-	soup.sweet = parent.base_soup_flavor_data.sweet
-	soup.sour = parent.base_soup_flavor_data.sour
-	soup.salty = parent.base_soup_flavor_data.salty
-	soup.umami = parent.base_soup_flavor_data.umami
-	soup.bitter = parent.base_soup_flavor_data.bitter
-	soup.numbing_spice = parent.base_soup_flavor_data.numbing_spice
-	soup.hot_spice = parent.base_soup_flavor_data.hot_spice
-	soup.richness = parent.base_soup_flavor_data.richness
-	soup.acidity = parent.base_soup_flavor_data.acidity
+	soup.sweet = parent._base_soup_flavor_data.sweet
+	soup.sour = parent._base_soup_flavor_data.sour
+	soup.salty = parent._base_soup_flavor_data.salty
+	soup.umami = parent._base_soup_flavor_data.umami
+	soup.bitter = parent._base_soup_flavor_data.bitter
+	soup.numbing_spice = parent._base_soup_flavor_data.numbing_spice
+	soup.hot_spice = parent._base_soup_flavor_data.hot_spice
+	soup.richness = parent._base_soup_flavor_data.richness
+	soup.acidity = parent._base_soup_flavor_data.acidity
 	
 	var setup_fn = func (x : Kitchen): x.setup_scene(soup)
 	Signals.emit_signal("load_new_scene", "res://Scenes/kitchen.tscn", true, setup_fn)

2f2116e4302b95ffc80a1daeb5894df18010bac5
Ryan Lavin <lavinrp@gmail.com>
2026-02-07 00:35:47 -0500

    fix view_recipe use of setup_scene

--- Scripts/view_recipe.gd
+++ Scripts/view_recipe.gd
@@ -29,7 +29,7 @@ func _on_pressed() -> void:
 	#var poison = get_parent().get_node("Poison")
 	
 	# The ingredients gets captured. Call this on the newly instatiated scene
-	var setup_fn = func (x : FlavorReport): x.setup_scene(_ingredients, _soup, _poison)
+	var setup_fn = func (x : FlavorReport): x.setup_scene(_ingredients, _soup, _poison, false)
 
 	
 	#Signals.emit_signal("load_new_scene", "res://Scenes/flavor_report.tscn", false, setup_fn)
